bd08a6c38cdf5da903d891e6917261ffe9eb603a
Wesley Gill <WesleyG95@hotmail.com>
2019-05-27 03:57:05 -0400

    Initial Commit
    Basic Tic Tac Toe game. Things to add:
    New game button
    AI
    Count wins/losses
    Save game

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTopLeft = new System.Windows.Forms.Button();
+            this.buttonTopMiddle = new System.Windows.Forms.Button();
+            this.buttonTopRight = new System.Windows.Forms.Button();
+            this.buttonMiddleLeft = new System.Windows.Forms.Button();
+            this.buttonMiddleMiddle = new System.Windows.Forms.Button();
+            this.buttonMiddleRight = new System.Windows.Forms.Button();
+            this.buttonBottomLeft = new System.Windows.Forms.Button();
+            this.buttonBottomMiddle = new System.Windows.Forms.Button();
+            this.buttonBottomRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTopLeft
+            // 
+            this.buttonTopLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTopLeft.Location = new System.Drawing.Point(12, 12);
+            this.buttonTopLeft.Name = "buttonTopLeft";
+            this.buttonTopLeft.Size = new System.Drawing.Size(167, 138);
+            this.buttonTopLeft.TabIndex = 0;
+            this.buttonTopLeft.TabStop = false;
+            this.buttonTopLeft.UseVisualStyleBackColor = true;
+            this.buttonTopLeft.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonTopMiddle
+            // 
+            this.buttonTopMiddle.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTopMiddle.Location = new System.Drawing.Point(185, 12);
+            this.buttonTopMiddle.Name = "buttonTopMiddle";
+            this.buttonTopMiddle.Size = new System.Drawing.Size(167, 138);
+            this.buttonTopMiddle.TabIndex = 1;
+            this.buttonTopMiddle.TabStop = false;
+            this.buttonTopMiddle.UseVisualStyleBackColor = true;
+            this.buttonTopMiddle.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonTopRight
+            // 
+            this.buttonTopRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTopRight.Location = new System.Drawing.Point(358, 12);
+            this.buttonTopRight.Name = "buttonTopRight";
+            this.buttonTopRight.Size = new System.Drawing.Size(167, 138);
+            this.buttonTopRight.TabIndex = 2;
+            this.buttonTopRight.TabStop = false;
+            this.buttonTopRight.UseVisualStyleBackColor = true;
+            this.buttonTopRight.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonMiddleLeft
+            // 
+            this.buttonMiddleLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMiddleLeft.Location = new System.Drawing.Point(12, 156);
+            this.buttonMiddleLeft.Name = "buttonMiddleLeft";
+            this.buttonMiddleLeft.Size = new System.Drawing.Size(167, 138);
+            this.buttonMiddleLeft.TabIndex = 3;
+            this.buttonMiddleLeft.TabStop = false;
+            this.buttonMiddleLeft.UseVisualStyleBackColor = true;
+            this.buttonMiddleLeft.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonMiddleMiddle
+            // 
+            this.buttonMiddleMiddle.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMiddleMiddle.Location = new System.Drawing.Point(185, 156);
+            this.buttonMiddleMiddle.Name = "buttonMiddleMiddle";
+            this.buttonMiddleMiddle.Size = new System.Drawing.Size(167, 138);
+            this.buttonMiddleMiddle.TabIndex = 4;
+            this.buttonMiddleMiddle.TabStop = false;
+            this.buttonMiddleMiddle.UseVisualStyleBackColor = true;
+            this.buttonMiddleMiddle.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonMiddleRight
+            // 
+            this.buttonMiddleRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMiddleRight.Location = new System.Drawing.Point(358, 156);
+            this.buttonMiddleRight.Name = "buttonMiddleRight";
+            this.buttonMiddleRight.Size = new System.Drawing.Size(167, 138);
+            this.buttonMiddleRight.TabIndex = 5;
+            this.buttonMiddleRight.TabStop = false;
+            this.buttonMiddleRight.UseVisualStyleBackColor = true;
+            this.buttonMiddleRight.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonBottomLeft
+            // 
+            this.buttonBottomLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBottomLeft.Location = new System.Drawing.Point(12, 300);
+            this.buttonBottomLeft.Name = "buttonBottomLeft";
+            this.buttonBottomLeft.Size = new System.Drawing.Size(167, 138);
+            this.buttonBottomLeft.TabIndex = 6;
+            this.buttonBottomLeft.TabStop = false;
+            this.buttonBottomLeft.UseVisualStyleBackColor = true;
+            this.buttonBottomLeft.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonBottomMiddle
+            // 
+            this.buttonBottomMiddle.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBottomMiddle.Location = new System.Drawing.Point(185, 300);
+            this.buttonBottomMiddle.Name = "buttonBottomMiddle";
+            this.buttonBottomMiddle.Size = new System.Drawing.Size(167, 138);
+            this.buttonBottomMiddle.TabIndex = 7;
+            this.buttonBottomMiddle.TabStop = false;
+            this.buttonBottomMiddle.UseVisualStyleBackColor = true;
+            this.buttonBottomMiddle.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // buttonBottomRight
+            // 
+            this.buttonBottomRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBottomRight.Location = new System.Drawing.Point(358, 300);
+            this.buttonBottomRight.Name = "buttonBottomRight";
+            this.buttonBottomRight.Size = new System.Drawing.Size(167, 138);
+            this.buttonBottomRight.TabIndex = 8;
+            this.buttonBottomRight.TabStop = false;
+            this.buttonBottomRight.UseVisualStyleBackColor = true;
+            this.buttonBottomRight.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 453);
+            this.Controls.Add(this.buttonBottomRight);
+            this.Controls.Add(this.buttonBottomMiddle);
+            this.Controls.Add(this.buttonBottomLeft);
+            this.Controls.Add(this.buttonMiddleRight);
+            this.Controls.Add(this.buttonMiddleMiddle);
+            this.Controls.Add(this.buttonMiddleLeft);
+            this.Controls.Add(this.buttonTopRight);
+            this.Controls.Add(this.buttonTopMiddle);
+            this.Controls.Add(this.buttonTopLeft);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTopLeft;
+        private System.Windows.Forms.Button buttonTopMiddle;
+        private System.Windows.Forms.Button buttonTopRight;
+        private System.Windows.Forms.Button buttonMiddleLeft;
+        private System.Windows.Forms.Button buttonMiddleMiddle;
+        private System.Windows.Forms.Button buttonMiddleRight;
+        private System.Windows.Forms.Button buttonBottomLeft;
+        private System.Windows.Forms.Button buttonBottomMiddle;
+        private System.Windows.Forms.Button buttonBottomRight;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool player1Turn = true;
+        int turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            bool gameOver = false;
+
+            try
+            {
+                Button buttonClicked = (Button)sender;
+
+                if(player1Turn)
+                {
+                    buttonClicked.Text = "X";
+                }
+                else
+                {
+                    buttonClicked.Text = "O";
+                }
+
+                buttonClicked.Enabled = false;
+
+                gameOver = gameWon();
+
+                if(gameOver)
+                {
+                    disableButtons();
+
+                    if(player1Turn)
+                    {
+                        MessageBox.Show("X wins!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O wins!");
+                    }
+                }
+                else if(turnCount >= 8)
+                {
+                    MessageBox.Show("The game was a tie!");
+                }
+                else
+                {
+                    turnCount++;
+                    player1Turn = !player1Turn;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private Boolean gameWon()
+        {
+            if(!buttonTopLeft.Enabled)
+            {
+                if((buttonTopLeft.Text.Equals(buttonTopMiddle.Text)) && (buttonTopMiddle.Text.Equals(buttonTopRight.Text)))
+                {
+                    //x x x
+                    //. . .
+                    //. . .
+                    return true;
+                }
+                else if((buttonTopLeft.Text.Equals(buttonMiddleLeft.Text)) && (buttonMiddleLeft.Text.Equals(buttonBottomLeft.Text)))
+                {
+                    //x . .
+                    //x . .
+                    //x . .
+                    return true;
+                }
+                else if((buttonTopLeft.Text.Equals(buttonMiddleMiddle.Text)) && (buttonMiddleMiddle.Text.Equals(buttonBottomRight.Text)))
+                {
+                    //x . .
+                    //. x .
+                    //. . x
+                    return true;
+                }
+            }
+
+            if(!buttonBottomRight.Enabled)
+            {
+                if((buttonBottomRight.Text.Equals(buttonMiddleRight.Text)) && (buttonMiddleRight.Text.Equals(buttonTopRight.Text)))
+                {
+                    //. . x
+                    //. . x
+                    //. . x
+                    return true;
+                }
+                else if((buttonBottomRight.Text.Equals(buttonBottomMiddle.Text)) && (buttonBottomMiddle.Text.Equals(buttonBottomLeft.Text)))
+                {
+                    //. . .
+                    //. . .
+                    //x x x
+                    return true;
+                }
+            }
+
+            if(!buttonMiddleMiddle.Enabled)
+            {
+                if((buttonMiddleMiddle.Text.Equals(buttonTopMiddle.Text)) && (buttonTopMiddle.Text.Equals(buttonBottomMiddle.Text)))
+                {
+                    //. x .
+                    //. x .
+                    //. x .
+                    return true;
+                }
+                else if((buttonMiddleMiddle.Text.Equals(buttonMiddleLeft.Text)) && (buttonMiddleLeft.Text.Equals(buttonMiddleRight.Text)))
+                {
+                    //. . .
+                    //x x x
+                    //. . .
+                    return true;
+                }
+                else if((buttonMiddleMiddle.Text.Equals(buttonBottomLeft.Text)) && (buttonBottomLeft.Text.Equals(buttonTopRight.Text)))
+                {
+                    //. . x
+                    //. x .
+                    //x . .
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void disableButtons()
+        {
+            foreach(Control c in Controls)
+            {
+                if(c is Button)
+                {
+                    c.Enabled = false;
+                }
+            }
+        }
+    }
+}
